fa34abed0eee3b03e85698229ebfc452fe59782c
adamveris2024 <adamveris2024@gmail.com>
2025-05-08 13:49:25 -0700

    Created PetAdoptionContext and added secrets.json

---
+++ Data/PetAdoptionContext.cs
@@ -0,0 +1,18 @@
+﻿using COMP003B.SP25.FinalProject.VerissimoA.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.SP25.FinalProject.VerissimoA.Data
+{
+    public class PetAdoptionContext : DbContext
+    {
+        public PetAdoptionContext(DbContextOptions<PetAdoptionContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Animal> Animals  { get; set; }
+        public DbSet<Shelter> Shelters { get; set; }
+        public DbSet<Staff> Members { get; set; }
+        public DbSet<Adopter> Adopters { get; set; }
+        public DbSet<AdoptionRequest> AdoptionRequests { get; set; }
+    }
+}
