cf5fe78281a08721e65dfc7d740cffb5b9144660
adamveris2024 <adamveris2024@gmail.com>
2025-05-08 13:18:02 -0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B.SP25.FinalProject.VerissimoA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
