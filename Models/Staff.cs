cb69405302a602adf6ea08f893e8250129724926
adamveris2024 <adamveris2024@gmail.com>
2025-05-12 21:14:31 -0700

    edited if entities in the staff model were null or not

--- Models/Staff.cs
+++ Models/Staff.cs
@@ -8,14 +8,14 @@ namespace COMP003B.SP25.FinalProject.VerissimoA.Models
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; } = null;
+        public string Name { get; set; }
 
         [Required]
-        public string Role { get; set; } = null;
+        public string Role { get; set; }
 
         [Required]
         [EmailAddress]
-        public string Email { get; set; } = null;
+        public string Email { get; set; }
 
         public int ShelterId { get; set; }
         public Shelter? Shelter { get; set; }
